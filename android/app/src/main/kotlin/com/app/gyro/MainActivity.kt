d8ed5545988f21f0561b44bb5ff7cf1647938de2
noahweasley <iebenmelu@gmail.com>
2024-11-13 12:44:49 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/app/gyro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.gyro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:gyro/app/app.dart';
+import 'package:gyro/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
